0111e969bad858b01767f26d72c322ea8f93f102
emico-git <emilcu05@gmail.com>
2022-08-02 18:15:23 +0200

    first design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import image from './images/logo.png'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='nav-header'>
+        <img className='logo' src={image} height={150} width={150}/>
+        <ul >
+          <div className='nav-links'>
+            <li><p>Über mich</p></li>
+            <li><p>Schulische Laufbahn</p></li>
+            <li><p>Kenntnisse & Fähigkeiten</p></li>
+            <li><p>Kontakt</p></li>
+          </div>
+        </ul>
+      </div>
+      <div className='uebermich'>
+        <div className='titel'>
+          <p>Übermich</p>
+        </div>
+      </div>
+      <div className='schulischeLaufbahn'>
+        <div className='titel'>
+          <p>Schulische Laufbahn</p>
+        </div>
+      </div>
+      <div className='kf'>
+        <div className='titel'>
+          <p>Kenntnisse & Fähigkeiten</p>
+        </div>
+      </div>
+      <div className='kontakt'>
+        <div className='titel'>
+          <p>Kontakt</p>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
